c742930159a9d2fb6f8f8ff322a8a938bbefd94e
roctbb <roctbb@gmail.com>
2022-12-17 23:37:07 +0300

    show board and pawn moves

--- src/interface/tkgame.py
+++ src/interface/tkgame.py
@@ -3,6 +3,7 @@ from interface.styles import DefaultTkBoardStyle
 from domain.board import Board
 from tkinter import Tk, Canvas
 
+
 class TkGame(GameEngine):
     def __init__(self):
         super().__init__()
@@ -12,15 +13,24 @@ class TkGame(GameEngine):
         self.board = Board()
         self.style = DefaultTkBoardStyle()
         self._active_turn_from = None
-
-        self.window.resizable(width=False, height=False)
+        self.window.rowconfigure(0, weight=1)
+        self.window.columnconfigure(0, weight=1)
         self.canvas = Canvas(self.window, width=640, height=640)
         self.canvas.bind("<Button-1>", self._click)
-        self.canvas.pack()
-
+        self.canvas.pack(fill="both", expand=True)
+        self.window.bind("<Configure>", self._window_resized)
         self._draw_board()
 
         self.window.mainloop()
+
+    def _window_resized(self, event):
+        height = event.width
+        width = event.height
+
+        if height <= 0 or width <= 0:
+            return
+
+        self._draw_board()
 
     def _click(self, event):
         x, y = event.x, event.y
@@ -44,21 +54,27 @@ class TkGame(GameEngine):
         self._draw_board()
 
     def _draw_board(self):
-        cell_width = int(self.canvas['width']) // self.board.width
-        cell_height = int(self.canvas['height']) // self.board.height
+        print(self.canvas['width'], self.canvas['height'], self.canvas.winfo_width())
+        cell_width = self.canvas.winfo_width() // self.board.width
+        cell_height = self.canvas.winfo_height() // self.board.height
+
+        if cell_width <= 1 or cell_height <= 1:
+            return
 
         for i in range(self.board.width):
             for j in range(self.board.width):
-                self.style.draw_cell((cell_width * i, cell_height * j, cell_width, cell_height), self.board.get_color((i, j)), self.canvas, self._active_turn_from == (i, j))
+                self.style.draw_cell((cell_width * i, cell_height * j, cell_width, cell_height),
+                                     self.board.get_color((i, j)), self.canvas, self._active_turn_from == (i, j))
 
                 if self.board.get((i, j)):
-                    self.style.draw_figure((cell_width * i, cell_height * j, cell_width, cell_height), self.board.get((i, j)), self.canvas)
+                    self.style.draw_figure((cell_width * i, cell_height * j, cell_width, cell_height),
+                                           self.board.get((i, j)), self.canvas)
 
     def _find_cell_coordinates(self, x, y):
-        cell_width = int(self.canvas['width']) // self.board.width
-        cell_height = int(self.canvas['height']) // self.board.height
+        cell_width = self.canvas.winfo_width() // self.board.width
+        cell_height = self.canvas.winfo_height() // self.board.height
 
-        i = x // cell_height
+        i = x // cell_width
         j = y // cell_height
 
         return (i, j)
