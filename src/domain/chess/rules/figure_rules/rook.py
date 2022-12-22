689da305863b4391193ee55a50868259e8c750c4
$sudo heck beluga <96012285+NRF24l01@users.noreply.github.com>
2022-12-22 06:11:22 -1100

    Update rook.py
    fix rook

--- src/domain/chess/rules/figure_rules/rook.py
+++ src/domain/chess/rules/figure_rules/rook.py
@@ -10,23 +10,33 @@ class RookRule(FigureRule):
     def can_move(points: List[Point], turn: Color, board: Board) -> bool:
         start, end = points
         if start[0] == end[0]:
+            print("17")
             if start[1] > end[1]:
+                print("21")
                 for i in range(end[1] + 1, start[1]):
                     if board.get((start[0], i)) is not None:
+                        print("11")
                         return False
             elif start[1] < end[1]:
-                for i in range(start[1], end[1] - 1):
+                print("16")
+                for i in range(start[1] + 1, end[1]):
                     if board.get((start[0], i)) is not None:
+                        print("12")
                         return False
             return True
         elif start[1] == end[1]:
+            print("22")
             if start[0] > end[0]:
+                print("23")
                 for i in range(end[0] + 1, start[0]):
-                    if board.get((start[1], i)) is not None:
+                    if board.get((i, start[1])) is not None:
+                        print("14", (start[1], i))
                         return False
             elif start[0] < end[0]:
-                for i in range(start[0], end[0] - 1):
-                    if board.get((start[1], i)) is not None:
+                print("26")
+                for i in range(end[0], start[0]):
+                    if board.get((i, start[1])) is not None:
+                        print("13")
                         return False
             return True
         return False
