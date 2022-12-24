5cd0cfbe50ce72e6f839612f913706caac4e90fa
artem4567815 <99493172+artem4567815@users.noreply.github.com>
2022-12-24 18:31:24 +0300

    Update bishop.py

--- src/domain/chess/rules/figure_rules/bishop.py
+++ src/domain/chess/rules/figure_rules/bishop.py
@@ -8,4 +8,29 @@ from domain.rules import FigureRule
 class BishopRule(FigureRule):
     @staticmethod
     def can_move(points: List[Point], turn: Color, board: Board) -> bool:
+        start, end = points
+
+        vx = 0
+        vy = 0
+        figure = board.get(start)
+        if not figure:
+            raise Exception("Invalid move")
+
+        if abs(start[0] - end[0]) == abs(start[1] - end[1]):
+            if end[0] < start[0]:
+                vx = -1
+            if end[1] < start[1]:
+                vy = -1
+            if end[0] > start[0]:
+                vx = 1
+            if end[1] > start[1]:
+                vy = -1
+
+            current = (start[0] + vx, start[1] + vy)
+            while current != end:
+                if board.get(current) is not None:
+                    return False
+                current = (current[0] + vx, current[1] + vy)
+            if not board.get(end) or board.get(end).color != figure.color:
+                return True
         return False
